db016e9d4ace22db43fb3d34a7c5477f2b07068e
prajjwolhumagain <prajjwolhumagain@gmail.com>
2025-02-26 11:57:08 -0600

    Created service class with methods to add, list and remove the tasks from queue

--- src/service/TaskQueueService.java
+++ src/service/TaskQueueService.java
@@ -1,5 +1,21 @@
 package service;
 
-public class TaskQueueService {
+import model.Task;
+import java.util.LinkedList;
+import java.util.Queue;
 
+public class TaskQueueService {
+    private Queue<Task> queue = new LinkedList<>();
+
+    public void enqueue(Task task) {
+        queue.add(task);
+    }
+
+    public Queue<Task> listTasks() {
+        return queue;
+    }
+
+    public Task dequeue() {
+        return queue.poll();
+    }
 }
