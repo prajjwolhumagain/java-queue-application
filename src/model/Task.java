0e78d4a30cfde2afa7c61e3bbb89a57d5babf92b
prajjwolhumagain <prajjwolhumagain@gmail.com>
2025-02-26 11:55:34 -0600

    Created Task Model with its attributes and required methods

--- src/model/Task.java
+++ src/model/Task.java
@@ -1,5 +1,24 @@
 package model;
 
 public class Task {
+    private int id;
+    private String description;
 
+    public Task(int id, String description) {
+        this.id = id;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{id=" + id + ", description='" + description + "'}";
+    }
 }
